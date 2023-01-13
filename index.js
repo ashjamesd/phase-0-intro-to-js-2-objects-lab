2d67eb392fd20234b863e7625f4e310135cb5ec1
Ash <ashjames.dean@gmail.com>
2023-01-13 15:05:03 -0500

    commit tree 110e02c9e6a68a65354b7de6df9c620dc6e2583e

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your solution in this file!
+const employee = {name:'ash', streetAddress:'monroe'};
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    let newEmployee = {...employee};
+    console.log(newEmployee);
+    newEmployee[key] = value;
+    console.log(newEmployee)
+    return newEmployee;
+    }
+const employee2 = updateEmployeeWithKeyAndValue(employee, 'name', 'sam');
